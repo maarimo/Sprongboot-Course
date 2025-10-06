ddf2b6957d43bea5490f1ba692fc7f6f2a3936b0
Matheus Amorim <abmm1234@hotmail.com>
2025-10-06 09:13:35 -0300

    first commit

---
+++ src/test/java/com/estudando/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudando.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
