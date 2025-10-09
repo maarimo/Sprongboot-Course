2730d260f32fef6447ee667ecc1ded07252286fe
Matheus Amorim <abmm1234@hotmail.com>
2025-10-09 05:06:04 -0300

    Subtotal and total

--- src/main/java/com/estudando/curso/entities/Order.java
+++ src/main/java/com/estudando/curso/entities/Order.java
@@ -87,6 +87,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal(){
+        Double sum = 0.0;
+        for (OrderItem x : items){
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
